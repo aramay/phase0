dc291568c6dead9d407a95f4620877b7b647f5ed
“kasper341” <kasper_341@hotmail.com>
2016-03-01 12:08:38 -0800

    Update 4.3

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -18,16 +18,49 @@ y = x + 1
 puts "bigger and better number is #{x}"
 
 =begin
+Release 4: More small exercises!
+
+Expand your brain a bit by working on the following exercises:
+
+Return a Formatted Address
+https://github.com/kasper341/phase0/tree/master/week-4/address
+
+Defining Math Methods
+https://github.com/kasper341/phase0/tree/master/week-4/math
+
 Release 6: Reflect
 
 answer the following questions:
 
 How do you define a local variable?
+Variables defined within a method are local variables.
 
+ans = 0
+def add(num_1, num_2)
+ #your code here
+ ans = num_1 + num_2
+ return ans
+end
+
+Here varaible 'ans' maintain two seperate copies, independent of each other.
 How do you define a method?
+
+It begind with keyword 'def' followed by method name. Ends with a keyword 'end' to denote that is closed.
+
+def add(num_1, num_2)
+
+end
+
 What is the difference between a local variable and a method?
+
 How do you run a ruby program from the command line?
+  we use a command
+  'ruby filename.rb'
 How do you run an RSpec file from the command line?
+  we use a command
+  'rspec filename.rb'
+
 What was confusing about this material? What made sense?
+This challenge did not have any surprises. Ii was pretty comfortable with all the materail.
 =end
 
