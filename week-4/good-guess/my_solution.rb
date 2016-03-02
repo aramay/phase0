2b10aaf56e9f19d1dd3936475d53111eded002ec
“kasper341” <kasper_341@hotmail.com>
2016-03-02 12:04:26 -0800

    Update PPS 4.5

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,16 @@
+# Good Guess
+
+# I worked on this challenge [by myself]
+
+
+# Your Solution Below
+
+def good_guess?(num)
+
+  if (num == 42)
+    return true
+  else
+    return false
+  end
+
+end
