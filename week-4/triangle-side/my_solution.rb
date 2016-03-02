c6e7319fbdb5a2f91e892a8bbc60ebbc3a15f43b
“kasper341” <kasper_341@hotmail.com>
2016-03-02 11:54:00 -0800

    Complete PPS

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -1,5 +1,5 @@
-=begind
-# I worked on this challenge [by myself, with: ].
+=begin
+# I worked on this challenge [with: Frank Lam].
 
 Theorem:
 the sum of two sides of a triangle must be greater than the third side. If this is true for all three combinations, then you will have a valid triangle.
@@ -9,13 +9,16 @@ the sum of two sides of a triangle must be greater than the third side. If this 
 
 def valid_triangle?(a, b, c)
   # Your code goes here!
+  if a + b > c
+    if a + c > b
+      if b + c > a
+        return true
+      end
+    end
 
-  if(a + b > c and b + c > a and c + a > b)
-    return true
   else
     return false
   end
-
 end
 
 puts valid_triangle?(0,0,0) # => false, because a triangle can't have 0-length sides
