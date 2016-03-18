fbbc5a89b1949ae3443c157cd24a285b28fa1c96
“kasper341” <kasper_341@hotmail.com>
2016-03-17 21:33:01 -0700

    Update validate-credit-card

--- week-6/validate-credit-card/my_solution.rb
+++ week-6/validate-credit-card/my_solution.rb
@@ -1,83 +1,121 @@
 # Class Warfare, Validate a Credit Card Number
 
 
-# I worked on this challenge [with: ].
-# I spent [1] hours on this challenge.
+# I worked on this challenge [by myself, with: ].
+# I spent [#] hours on this challenge.
 
 # Pseudocode
 
-# Input:
-# Output:
+# Input: An Integer
+# Output: True if valid number or False if invalid number
 # Steps:
+=begin
+1) Initialize class with credit card number
+2) Pass it to a method to convert the number to an array
+3) Check length of array to see if it is 16 digits and return an error if it is not
+4) Pass the validation array to a number checking method
+5) Reverse validation array
+6) itterate array doubling every other element
+6a) check if doubled number is > 9 then split and add
+7) Add all elements together
+8) Divide by 10 and there should be no remainder  (number%10 == 0)
+9) Return true IF == 0 ELSE false
+=end
 
 
 # Initial Solution
 
 # Don't forget to check on initialization for a card length
 # of exactly 16 digits
-require "pry"
 
 class CreditCard
+  def initialize(number)
+    @number=number
 
-  def initialize(credit_card_number)
-
-    @credit_card_number = credit_card_number.split(//)
-
-    @credit_card_number.delete(" ")
+    check_card
 
   end
 
-  #binding.pry
+  def check_card
+    validation_array = @number.to_s.split(//)
+    if validation_array.length != 16
+      raise ArgumentError.new
+    end
+    check_number(validation_array.reverse!)
+  end
 
-  def check_card()
-
-    # result_array = []
-
-    # p i = @credit_card_number.length - 1
-
-    # while (i > 0)
-
-    #   puts result_array = @credit_card_number[i-1].to_i + @credit_card_number[i-1].to_i
-    #   i -= 1
-    # end
-
-    # p @credit_card_number
-
-    @credit_card_number.each_with_index do |item, index|
-
-
-
-      if (index % 2 != 0)
-        p "yyes"
-        p "item #{item} with index #{index}"
-
-        p "add items #{item.to_i + item.to_i}"
-
-        @credit_card_number[index] = item.to_i + item.to_i
-
-
-
-
-
+  def check_number(array)
+    check_array=[]
+    array.each_with_index do |item , index|
+      if index.odd?
+        check_array[index] = item.to_i * 2
+#        if check_array[index] > 9
+#          check_array[index].to_s.split(//)
+      else
+        check_array[index] = item.to_i
       end
 
-      puts "#{@credit_card_number}"
+    end
+    sum_array(check_array)
+  end
 
+  def sum_array(array)
+    sum = 0
+    array.each_index do |index|
+      if array[index] > 9
+        array[index]=array[index].to_s.split(//)
+      end
+    end
+
+    print array
+
+    # array.each_with_index do |x , index|
+    #   if x.is_a?(Array)
+    #     array_sum = x[index][0].to_i + x[index][1].to_i
+    #     sum += array_sum
+
+    array.each do |x|
+      if x.kind_of?(Array)
+        x.each do |y|
+          sum += y.to_i
+        end
+      else
+        sum += x
+      end
+      puts sum
+    end
+
+    if sum % 10 == 0
+      return true
+    else
+      return false
     end
 
 
 
-
   end
-
 
 end
 
+# card1=CreditCard.new(1234567891234567)
+# card2=CreditCard.new(4563960122001999)
+# card3=CreditCard.new(1234)
 
-credit_card_number = "4563 9601 2200 1999"
-cc = CreditCard.new(credit_card_number)
+# puts card1.check_card
+# puts card2.check_card
 
-cc.check_card
+# puts card3.check_card
+
+# begin
+#   puts card3.check_card
+# rescue ArgumentError
+#   puts "exception raised"
+# end
+
+
+
+
+
 # Refactored Solution
 
 
