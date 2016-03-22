2c563225b4795119667c57b09e4b9e5f5d104853
“kasper341” <kasper_341@hotmail.com>
2016-03-22 14:44:47 -0700

    Update 7.3-challenge

---
+++ week-7/variables_objects.js
@@ -0,0 +1,81 @@
+ // JavaScript Variables and Objects
+
+// I paired [by myself] on this challenge.
+
+// __________________________________________
+// Write your code below.
+
+var secretNumber = 7;
+var password = "just open the door";
+var members = ["John","","","Mary"];
+var allowedIn = false;
+
+
+
+
+// __________________________________________
+
+// Test Code:  Do not alter code below this line.
+
+function assert(test, message, test_number) {
+  if (!test) {
+    console.log(test_number + "false");
+    throw "ERROR: " + message;
+  }
+  console.log(test_number + "true");
+  return true;
+}
+
+assert(
+  (typeof secretNumber === 'number'),
+  "The value of secretNumber should be a number.",
+  "1. "
+)
+
+assert(
+  secretNumber === 7,
+  "The value of secretNumber should be 7.",
+  "2. "
+)
+
+assert(
+  typeof password === 'string',
+  "The value of password should be a string.",
+  "3. "
+)
+
+assert(
+  password === "just open the door",
+  "The value of password should be 'just open the door'.",
+  "4. "
+)
+
+assert(
+  typeof allowedIn === 'boolean',
+  "The value of allowedIn should be a boolean.",
+  "5. "
+)
+
+assert(
+  allowedIn === false,
+  "The value of allowedIn should be false.",
+  "6. "
+)
+
+assert(
+  members instanceof Array,
+  "The value of members should be an array",
+  "7. "
+)
+
+assert(
+  members[0] === "John",
+  "The first element in the value of members should be 'John'.",
+  "8. "
+)
+
+assert(
+  members[3] === "Mary",
+  "The fourth element in the value of members should be 'Mary'.",
+  "9. "
+)
