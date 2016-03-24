ee8a21e246271e689ca42fb3379da21a19a05130
“kasper341” <kasper_341@hotmail.com>
2016-03-24 09:45:09 -0700

    Create directory for game challenge

---
+++ week-7/design-basic-game-solo-challenge/my_solution.js
@@ -0,0 +1,55 @@
+ // Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission: collect gems, don't step on spikes
+// Goals: collect gems
+// Characters: warrior
+// Objects: warrior, gems, spikes
+// Functions: moveWarrior, collectGems, StepOnSpikes
+
+// Pseudocode
+//
+//
+//
+//
+//
+
+// Initial Code
+function Gems(position){
+  this.position = position
+}
+
+function Spikes(position){
+
+}
+
+function Warrior(move){
+  this.move = move;
+}
+
+
+
+
+var dragonWarrior = new Warrior(12)
+
+console.log(dragonWarrior.move)
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
