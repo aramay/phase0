db964380fe8a613a100e42d38d5316aceff8ddd0
“kasper341” <kasper_341@hotmail.com>
2016-04-07 19:47:50 -0700

    Update 9.5 pairing challenge

---
+++ week-9/9.5/cipher.rb
@@ -0,0 +1,119 @@
+# Cipher Challenge
+
+# I worked on this challenge [by myself, with:]
+# I spent [#] hours on this challenge.
+
+# 1. Solution
+# Write your comments on what each thing is doing.
+# If you have difficulty, go into IRB and play with the methods.
+
+=begin
+def DrEvilsCipher(coded_message)
+  input = coded_message.downcase.split("") # Check out this method in IRB to see how it works! Also refer to the Ruby docs.
+  decoded_sentence = []
+  cipher = {"e" => "a",   # This is technically a shift of four letters...Can you think of a way to automate this? Is a hash
+            "f" => "b",   # the best data structure for this problem? What are the pros and cons of hashes?
+            "g" => "c",
+            "h" => "d",
+            "i" => "e",
+            "j" => "f",
+            "k" => "g",
+            "l" => "h",
+            "m" => "i",
+            "n" => "j",
+            "o" => "k",
+            "p" => "l",
+            "q" => "m",
+            "r" => "n",
+            "s" => "o",
+            "t" => "p",
+            "u" => "q",
+            "v" => "r",
+            "w" => "s",
+            "x" => "t",
+            "y" => "u",
+            "z" => "v",
+            "a" => "w",
+            "b" => "x",
+            "c" => "y",
+            "d" => "z"}
+
+  input.each do |x| # What is #each doing here?
+    found_match = false  # Why would this be assigned to false from the outset? What happens when it's true?
+    cipher.each_key do |y| # What is #each_key doing here?
+      if x == y  # What is this comparing? Where is it getting x? Where is it getting y? What are those variables really?
+        decoded_sentence << cipher[y]
+        found_match = true
+        break  # Why is it breaking here?
+      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" #What the heck is this doing?
+        decoded_sentence << " "
+        found_match = true
+        break
+      elsif (0..9).to_a.include?(x) # Try this out in IRB. What does   " (0..9).to_a "    do?
+        decoded_sentence << x
+        found_match = true
+        break
+      end
+    end
+    if not found_match  # What is this looking for?
+      decoded_sentence << x
+    end
+  end
+  decoded_sentence = decoded_sentence.join("")
+  #What is this method returning?
+end
+
+=end
+
+# Your Refactored Solution
+def dr_evils_cipher(coded_message)
+  # change coded_message into array of single character
+  encoded_message = coded_message.downcase.split("") # Check out this method in IRB to see how it works! Also refer to the Ruby docs.
+
+  #empty array to store decoded message
+  @decoded_sentence = []
+
+  # a dictonary to decode each letter with a shift of four characters
+  cipher = []
+
+  ("a".."z").each do |letter|
+    cipher << letter
+  end
+
+  space_array = ["@" , "#" , "$" , "%" , "^" , "&" , "*"]
+
+  encoded_message.each do |item|
+
+    if (cipher.include?(item))
+      @decoded_sentence << cipher[(cipher.index(item) - 4)%26]
+    else if (space_array.include?(item))
+      @decoded_sentence << " "
+    else
+      @decoded_sentence << item
+    end
+  end
+end
+  p @decoded_sentence.join("")
+end
+
+
+# Driver Test Code:
+
+# DrEvilsCipher.new("i want a coke!")
+
+# DrEvilsCipher.new("m^aerx%e&gsoi!") == "i want a coke!" #This is driver test code and should print true
+# Find out what Dr. Evil is saying below and turn it into driver test code as well. Driver test code statements should always return "true."
+
+
+
+DrEvilsCipher.new("syv%ievpc#exxiqtxw&ex^e$xvegxsv#fieq#airx%xlvsykl$wizivep#tvitevexmsrw.#tvitevexmsrw#e*xlvsykl#k&aivi%e@gsqtpixi&jempyvi.
+&fyx%rsa,$pehmiw@erh#kirxpiqir,%ai%jmreppc@lezi&e&asvomrk%xvegxsv#fieq,^almgl^ai^wlepp%gepp@tvitevexmsr^l")
+
+
+# p DrEvilsCipher("csy&wii,@m'zi@xyvrih$xli*qssr$mrxs&alex@m#pmoi%xs#gepp%e^hiexl#wxev.")
+# p DrEvilsCipher("qmrm#qi,*mj^m#iziv^pswx#csy#m^hsr'x%orsa^alex@m%asyph^hs.
+# @m'h%tvsfefpc%qszi$sr%erh*kix#ersxliv$gpsri@fyx*xlivi@asyph^fi@e^15&qmryxi@tivmsh%xlivi$alivi*m*asyph&nywx^fi$mrgsrwspefpi.")
+# p DrEvilsCipher("alc@qeoi*e$xvmppmsr^alir#ai*gsyph%qeoi...#fmppmsrw?")
+
+# Reflection
+# Keep your reflection limited to 10-15 minutes!
