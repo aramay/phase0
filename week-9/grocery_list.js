7c85fbb0072e4d10cad86bea1cf7316f5b32793d
“kasper341” <kasper_341@hotmail.com>
2016-04-08 15:40:20 -0700

    Update grocery challenge

--- week-9/grocery_list.js
+++ week-9/grocery_list.js
@@ -3,9 +3,21 @@
 grocery_list = {}
 
 function addItem(item, quantity){
-  // grocery_list[eggs] = 2
-  grocery_list[item] = quantity
+  //update item quantity if exist
+  if (grocery_list.hasOwnProperty(item)){
+    new_quantity = grocery_list[item];
+    console.log(new_quantity)
+    grocery_list[item] = new_quantity + quantity;
+  }
+  //add new entry
+  else
+    grocery_list[item] = quantity;
 }
+//remove item
+function removeItem(item){
+  delete grocery_list[item];
+}
+
 
 console.log(grocery_list)
 
@@ -14,6 +26,19 @@ quantity = 2
 
 addItem(item, quantity)
 
+console.log(grocery_list)
 
+addItem(item, 3)
+
+addItem("milk", 3)
 
 console.log(grocery_list)
+
+removeItem(item)
+
+console.log(grocery_list)
+
+//formatted output
+for (var x in grocery_list){
+  console.log("Your list includes "+x +" "+grocery_list[x]);
+}
